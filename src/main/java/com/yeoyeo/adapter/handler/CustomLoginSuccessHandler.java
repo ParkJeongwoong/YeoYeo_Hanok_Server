b58445579fddaa159d1b919a20f7f818215494e0
dvlprjw <dvlprjw@gmail.com>
2023-03-29 21:50:51 +0900

    Cookie 읽기를 위해 Samesite = None 변경

--- src/main/java/com/yeoyeo/adapter/handler/CustomLoginSuccessHandler.java
+++ src/main/java/com/yeoyeo/adapter/handler/CustomLoginSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.yeoyeo.adapter.handler;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
 
 @Component
 public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
@@ -18,6 +20,20 @@ public class CustomLoginSuccessHandler extends SavedRequestAwareAuthenticationSu
         // Spring Context Holder에 인증 정보 저장
         SecurityContextHolder.getContext().setAuthentication(authentication);
         response.setStatus(HttpServletResponse.SC_OK);
+        addSameSiteCookieAttribute(response);
+    }
+
+    private void addSameSiteCookieAttribute(HttpServletResponse response) {
+        Collection<String> headers = response.getHeaders(HttpHeaders.SET_COOKIE);
+        boolean firstHeader = true;
+        for (String header : headers) {
+            if (firstHeader) {
+                response.setHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "SameSite=None"));
+                firstHeader = false;
+                continue;
+            }
+            response.addHeader(HttpHeaders.SET_COOKIE, String.format("%s; %s", header, "SameSite=None"));
+        }
     }
 
 }
