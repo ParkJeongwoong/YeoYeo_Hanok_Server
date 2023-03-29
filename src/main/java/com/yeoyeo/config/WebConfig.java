1999116c35e3d9eb25478d3085d098f48aea7249
dvlprjw <dvlprjw@gmail.com>
2023-03-30 02:09:00 +0900

    SameSite=None 이슈 해결

--- src/main/java/com/yeoyeo/config/WebConfig.java
+++ src/main/java/com/yeoyeo/config/WebConfig.java
@@ -1,16 +1,23 @@
-//package com.yeoyeo.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:8080", "http://localhost:3005", "http://3.35.98.5:8080/", "https://yeoyeo.co.kr", "https://www.yeoyeo.co.kr");
-//    }
-//
-//}
+package com.yeoyeo.config;
+
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setSameSite("none");
+        serializer.setUseSecureCookie(true);
+        return serializer;
+    }
+
+}
