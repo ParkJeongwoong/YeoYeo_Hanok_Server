8a3556675ac485ebbcec39423f67cea44e511bf6
dvlprjw <dvlprjw@gmail.com>
2023-06-16 13:11:11 +0900

    temp. 관리자 페이지 모바일 테스트 용 임시 권한 변경

--- src/main/java/com/yeoyeo/config/SecurityConfig.java
+++ src/main/java/com/yeoyeo/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests() // 보호된 리소스 URI에 접근할 수 있는 권한 설정
 //                    .antMatchers("/admin/login", "/admin/logout", "/dateroom/**", "/calendar/**", "/guest/**","/payment/**","/reservation/**", "/login**").permitAll()
                     .antMatchers("/admin/**", "/swagger-ui/**", "/adminManage.html", "/index.html", "/").authenticated() // 인증된 사용자만 접근 허용
+                .antMatchers("/admin/reservation/list/**").permitAll()
                     .anyRequest().permitAll()
 //                    .anyRequest().authenticated()
                 .and()
