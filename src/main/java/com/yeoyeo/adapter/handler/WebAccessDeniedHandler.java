dfe724b99e32342e9a2f3b53617b74e65f843e2c
dvlprjw <dvlprjw@gmail.com>
2023-06-20 18:34:51 +0900

    fix. Admin 쪽 권한 변경

--- src/main/java/com/yeoyeo/adapter/handler/WebAccessDeniedHandler.java
+++ src/main/java/com/yeoyeo/adapter/handler/WebAccessDeniedHandler.java
@@ -1,5 +1,7 @@
 package com.yeoyeo.adapter.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -18,6 +20,8 @@ public class WebAccessDeniedHandler implements AccessDeniedHandler {
         // 방법1. 권한이 없는 경우 페이지 이동 시 사용
 //        response.sendRedirect("/login.html");
         // 방법2. 권한이 없는 경우 에러코드 반환 시 사용
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
--- src/main/java/com/yeoyeo/adapter/handler/WebAuthenticationEntryPoint.java
+++ src/main/java/com/yeoyeo/adapter/handler/WebAuthenticationEntryPoint.java
@@ -1,5 +1,7 @@
 package com.yeoyeo.adapter.handler;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -19,7 +21,9 @@ public class WebAuthenticationEntryPoint implements AuthenticationEntryPoint {
         // 방법1. 인증되지 않은 경우 페이지 이동 시 사용
 //        response.sendRedirect("/login.html");
         // 방법2. 인증되지 않은 경우 에러코드 반환 시 사용
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
     }
 
 }
