57cbba8e5cbd0dccd4889c750b1819b2c5140699
dvlprjw <dvlprjw@gmail.com>
2023-12-16 02:15:02 +0900

    fix: fetch join에 Payment 추가

--- src/main/java/com/yeoyeo/application/reservation/repository/ReservationRepository.java
+++ src/main/java/com/yeoyeo/application/reservation/repository/ReservationRepository.java
@@ -8,13 +8,13 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     @Override
-    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest"})
+    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest", "payment"})
     List<Reservation> findAll();
-    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest"})
+    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest", "payment"})
     List<Reservation> findAllByReservationState(long reservationState);
-    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest"})
+    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest", "payment"})
     List<Reservation> findAllByReservationStateAndReservedFrom(long reservationState, String reservedFrom);
-    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest"})
+    @EntityGraph(attributePaths = {"mapDateRoomReservations","mapDateRoomReservations.dateRoom", "mapDateRoomReservations.dateRoom.room", "guest", "payment"})
     List<Reservation> findAllByUniqueId(String uniqueId);
     @Query(value = "SELECT * FROM reservation WHERE first_date <= ?1 AND reserved_from = ?2", nativeQuery = true)
     List<Reservation> searchReservation(String searchWord, String phoneNumber);
