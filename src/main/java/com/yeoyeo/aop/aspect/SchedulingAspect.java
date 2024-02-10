51a88fced021a2d58af01c5ccc1b624d62f5b454
dvlprjw <dvlprjw@gmail.com>
2024-02-11 00:36:07 +0900

    feat: SingleJob annotation

---
+++ src/main/java/com/yeoyeo/aop/aspect/SchedulingAspect.java
@@ -0,0 +1,47 @@
+package com.yeoyeo.aop.aspect;
+
+import com.yeoyeo.application.common.method.CommonMethod;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Aspect
+@Component
+public class SchedulingAspect {
+
+    @Autowired
+    private CommonMethod commonMethod;
+
+    @Pointcut("execution(* com.yeoyeo.application..*Scheduler.*(..))")
+    private void schedulerPointcut() {}
+    @Pointcut("execution(* com.yeoyeo.application..*Generator.*(..))")
+    private void generatorPointcut() {}
+    @Pointcut("@annotation(com.yeoyeo.aop.annotation.SingleJob)")
+    private void singleJobPointcut() {}
+
+    @Transactional
+    @Around("schedulerPointcut() || generatorPointcut() || singleJobPointcut()")
+    public void schedulerLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+        String scheduleName = joinPoint.getSignature().getName();
+        try {
+            log.info("=== Server Profile : {} / Scheduler : {} ===", commonMethod.getServerProfile(), scheduleName);
+            commonMethod.startScheduling(scheduleName);
+            joinPoint.proceed();
+            commonMethod.endScheduling(scheduleName);
+            log.info("-- Scheduler : {} [SUCCESS] --", scheduleName);
+        } catch (Exception exception) {
+            log.error("-- Scheduler : {} [ERROR] (1) --", scheduleName);
+            log.error("-- Scheduler : {} [ERROR] (2) --", exception.getMessage());
+            log.error("-- Scheduler : {} [ERROR] (3) --", exception);
+        } finally {
+            log.info("=== Scheduler : {} [END] ===", scheduleName);
+        }
+    }
+
+}
