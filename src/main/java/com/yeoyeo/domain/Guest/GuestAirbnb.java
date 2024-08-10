0c86dd481b88af93f2866731827072dd87186f8c
dvlprjw <dvlprjw@gmail.com>
2024-08-10 23:48:06 +0900

    feat: add last 4 digits

--- src/main/java/com/yeoyeo/domain/Guest/GuestAirbnb.java
+++ src/main/java/com/yeoyeo/domain/Guest/GuestAirbnb.java
@@ -18,13 +18,15 @@ public class GuestAirbnb extends Guest {
         super.name = "AirBnbGuest";
     }
 
-    private GuestAirbnb(String name) {
+    private GuestAirbnb(String name, String last4Digits) {
         super.name = name;
+        super.phoneNumber = last4Digits;
     }
 
     public GuestAirbnb(Description description, Summary summary) {
         if (description == null || summary == null || !summary.getValue().equals("Reserved")) super.name = "AirBnbGuest_External";
         else super.name = "AirBnbGuest";
+        if (description != null) super.phoneNumber = getLast4DigitsFromAirbnb(description.getValue());
     }
 
     @Override
@@ -40,12 +42,21 @@ public class GuestAirbnb extends Guest {
 
     @Override
     public GuestAirbnb clone() {
-        return new GuestAirbnb(this.name);
+        return new GuestAirbnb(this.name, this.phoneNumber);
     }
 
     @Override
     public String getPlatformName() {
         return "airbnb.com";
+    }
+
+    private String getLast4DigitsFromAirbnb(String description) {
+        if (description == null || description.length() < 4) {
+            // 입력이 null이거나 길이가 4보다 작으면 전체 문자열 반환
+            return description;
+        }
+        // 마지막 4자리를 반환
+        return description.substring(description.length() - 4);
     }
 
 }
